34f8193358a09c18667491dbb497f5ee71c45e92
Guilherme Diel <guilherme@pop-os.localdomain>
2021-03-11 16:16:44 -0300

    meu primeiro commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :articles 
+      get 'welcome/index'
+    
+    root 'welcome#index'
+  
+    resources :articles do
+      resources :comments
+    end
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
